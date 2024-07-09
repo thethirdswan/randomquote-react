958c4a4072c14f774712b0edef29da63af53f903
TheThirdSwan <aryaswan17@gmail.com>
2024-07-09 23:59:54 +0800

    fixed quote to able to tweet first generated quote

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import './App.css';
 import { useState } from 'react';
 
-const quotes = [["'Fight for all that is beautiful in this world'", "miHoYo"], ["'May you the beauty of this world always shine'", "Hoyoverse"], ["'Just Do It'", "Nike"], ["'This is the beginning and the end'", "Nameless Deity"], ["'Nah, I don't really feel like it.'", "Spongebob"]]
+const quotes = [["'Fight for all that is beautiful in this world'", "miHoYo"], ["'May you the beauty of this world always shine'", "Hoyoverse"], ["'Just Do It'", "Nike"], ["'This is the beginning and the end'", "Nameless Deity"], ["'Nah, I don't really feel like it.'", "Spongebob"], ["'I will end this imperfect story into how we always wanted it to be.'", "Kiana Kaslana"]]
 
 function App() {
-  const newQuote = Math.floor(Math.random() * 5);
+  const newQuote = Math.floor(Math.random() * 6);
   const quoteNumber = quotes[newQuote]
   const [quote, setQuote] = useState(quoteNumber[0]);
   const [author, setAuthor] = useState(quoteNumber[1]);
-  const [tweet, setTweet] = useState("https://twitter.com/intent/tweet")
+  const [tweet, setTweet] = useState(`https://twitter.com/intent/tweet?text=${quoteNumber[0]}, ${quoteNumber[1]}`)
   function quoteGenerator() {
-    const quoteNumber = Math.floor(Math.random() * 5);
+    const quoteNumber = Math.floor(Math.random() * 6);
     const chosenQuote = quotes[quoteNumber]
     setQuote(chosenQuote[0]);
     setAuthor(chosenQuote[1]);
-    setTweet(`https://twitter.com/intent/tweet?text=${chosenQuote[0]},${chosenQuote[1]}`)
+    setTweet(`https://twitter.com/intent/tweet?text=${chosenQuote[0]}, ${chosenQuote[1]}`)
   }
   return (
     <div id="quote-box">
